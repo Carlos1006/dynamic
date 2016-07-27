0aabb479d1d500df1ca7101d24ca1a68924c95c4
Naker21 <cdgzz19@gmail.com>
2016-07-26 20:15:09 -0500

    Links en header y correcciones esteticas
    Se corrigieron multiples alineamientos de simuladores y home.
    Se agregaron links de redes sociales en el header
    Se agrego el link a home en el icono de la empresa.
    Se modificaron los botones a enlaces (tag a)
    funcionalidad de mail en contacto pendiente

--- include/header.php
+++ include/header.php
@@ -44,8 +44,11 @@ switch($page) {
     <div class="col_1" id="contact">
         <span class="spanMiddle" id="mailContacto">contacto@dynamicsimulation.com.mx</span>
         <span class="spanMiddle" id="telefono">T. +52 (81) 8335 8514</span>
+        <a class="spanMiddle" href="https://www.facebook.com/DynamicSimulation/" id="facebook"></a>
+        <a class="spanMiddle" href="https://www.youtube.com/channel/UCBCgeNNmQ1f5nmkA5-pJ_LQ" id="twitter"></a>
     </div>
     <span class="col_2" id="dynamicIcon"></span>
+    <a id="linkerToMain" href="<?php echo $href["dynamic"] ?>"></a>
     <div class="col_10 navIcon link" href="<?php echo $href["dynamic"] ?>"     <?php echo $attr["dynamic"] ?>     id="inicioIcon"><label>Inicio</label></div>
     <div class="col_10 navIcon link" href="<?php echo $href["nosotros"] ?>"    <?php echo $attr["nosotros"] ?>    id="nosotrosIcon"><label>Nosotros</label></div>
     <div class="col_10 navIcon link" href="<?php echo $href["simuladores"] ?>" <?php echo $attr["simuladores"] ?> id="simuladoresIcon"><label>Simuladores</label></div>
--- js/linker.js
+++ js/linker.js
@@ -1,1 +1,1 @@
-$(function() {		$(".link").click(function(){		document.location.href = $(this).attr("href");	});	});
+$(function() {		$(".link").click(function(){		document.location.href = $(this).attr("href");	});	$("#facebook,#twitter").click(function(e) {		e.preventDefault();		var win = window.open($(this).attr("href"), '_blank');		win.focus();	});	});
